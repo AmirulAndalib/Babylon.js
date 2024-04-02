acdb008a298e68856646b6ac89d05a8db740c3dd
Deltakosh <david.catuhe@live.fr>
2024-04-02 15:15:43 -0700

    Address comments

--- packages/dev/core/src/Meshes/GreasedLine/greasedLineBaseMesh.ts
+++ packages/dev/core/src/Meshes/GreasedLine/greasedLineBaseMesh.ts
@@ -8,6 +8,7 @@ import { VertexData } from "../mesh.vertexData";
 import { DeepCopier } from "../../Misc/deepCopier";
 import { GreasedLineSimpleMaterial } from "../../Materials/GreasedLine/greasedLineSimpleMaterial";
 import type { Engine } from "../../Engines/engine";
+import type { FloatArray, IndicesArray } from "../../types";
 
 /**
  * In POINTS_MODE_POINTS every array of points will become the center (backbone) of the ribbon. The ribbon will be expanded by `width / 2` to `+direction` and `-direction` as well.
